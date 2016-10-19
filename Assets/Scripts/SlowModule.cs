f07e55d21c3a46dfae09b6f82e6677b9c4e2536d
Tinaud <mathieu.naud@hotmail.com>
2016-10-19 14:35:59 -0400

    Ajout de ma scène et prefabs des modules (-ralentissement des enemies   -blocage des ennemies   -Meurtre des ennemies)

---
+++ Assets/Scripts/SlowModule.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlowModule : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other) {
+        other.gameObject.GetComponent<Enemy>().SetSpeed(2f);
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        other.gameObject.GetComponent<Enemy>().SetSpeed(5f);
+    }
+}
