1a8c2b8b857ab9e75b6edf19f2495547f5fa8347
AntoineDutil <antoineberthiaumedutil@gmail.com>
2016-10-24 21:31:14 -0400

    Fais le choix des modules avec l'interface et on peut aussi les placer.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour {
     public float m_speed = 1f;
     public float j_force = 1f;
     public AudioClip jumpSound;
+    public GameObject[] listModule;
 
     private Rigidbody2D rBody;
     private AudioSource source;
@@ -38,6 +39,22 @@ public class PlayerController : MonoBehaviour {
             {
                 source.PlayOneShot(jumpSound, 1);
                 rBody.AddForce(new Vector2(0, j_force));
+            }
+            else if (Input.GetKeyDown(KeyCode.Z))
+            {
+                if (GameManager.instance.IndexItem == 2)
+                {
+                    GameManager.instance.IndexItem = 0;
+                }
+                else
+                {
+                    GameManager.instance.IndexItem++;
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.X))
+            {
+                source.PlayOneShot(jumpSound, 1);
+                Instantiate(listModule[GameManager.instance.IndexItem], transform.position, Quaternion.identity);
             }
         }
     }
---
+++ Assets/Scripts/UiModulesController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UiModulesController : MonoBehaviour {
+
+    public Sprite[] ListSprite;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(GameManager.instance.IndexItem == 0)
+        {
+            GetComponent<Image>().sprite = ListSprite[0];
+        }
+        else if (GameManager.instance.IndexItem == 1)
+        {
+            GetComponent<Image>().sprite = ListSprite[1];
+        }
+        else if (GameManager.instance.IndexItem == 2)
+        {
+            GetComponent<Image>().sprite = ListSprite[2];
+        }
+	}
+}
