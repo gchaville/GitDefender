d65cb0f1be6a72498eebf4b409071de7352f96f0
youssefmoutaki <youssef.moutaki@gmail.com>
2016-10-29 13:37:35 -0500

    Create start, exit button, pause and timer

---
+++ Assets/Scripts/scrollingBackground.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scrollingBackground : MonoBehaviour
+{
+
+    public float speed = 0.5f;
+    Renderer renderer;
+
+    // Use this for initialization
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector2 offset = new Vector2(Time.time * speed, 0);
+        renderer.material.mainTextureOffset = offset;
+
+    }
+}
+
+
+
