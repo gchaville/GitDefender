b12592ac4a6a45137f449d5b21e7e57355f707f1
Hugo Bombail <hugo.bombail@wanadoo.fr>
2016-10-31 18:30:12 -0400

    - Amélioration de l'effet de glitch - Ajout du troisième ennemi

--- Assets/Scripts/HugoScripts/GlitchEffect.cs
+++ Assets/Scripts/HugoScripts/GlitchEffect.cs
@@ -4,12 +4,14 @@ using System.Collections;
 public class GlitchEffect : MonoBehaviour {
 
 	public GameObject glitchObject;
+	public Sprite[] glitchSprites;
 
 	public void launchGlitch(){
 		int nbGlitch = (int)Random.Range (20f, 50f);
 		for (int i = 0; i < nbGlitch; i++) {
 			GameObject toInstantiate = Instantiate (glitchObject, this.transform.position, Quaternion.identity) as GameObject;
 			toInstantiate.GetComponent<SpriteRenderer> ().color = new Color(Random.Range (0f, 1f), Random.Range (0f, 1f), Random.Range (0f, 1f));
+			toInstantiate.GetComponent<SpriteRenderer>().sprite = glitchSprites[(int)Random.Range(0f, glitchSprites.Length)];
 			toInstantiate.transform.position = new Vector3 (Random.Range (-9f, 9f), Random.Range (-5f, 5f), 0f);
 			toInstantiate.transform.localScale = new Vector3 (Random.Range (0f, 2f), Random.Range (0f, 2f), 0f);
 			Destroy(toInstantiate, Random.Range(0.1f, 0.5f));
@@ -21,6 +23,7 @@ public class GlitchEffect : MonoBehaviour {
 		for (int i = 0; i < nbGlitch; i++) {
 			GameObject toInstantiate = Instantiate (glitchObject, this.transform.position, Quaternion.identity) as GameObject;
 			toInstantiate.GetComponent<SpriteRenderer> ().color = new Color(Random.Range (0f, 1f), Random.Range (0f, 1f), Random.Range (0f, 1f));
+			toInstantiate.GetComponent<SpriteRenderer>().sprite = glitchSprites[(int)Random.Range(0f, glitchSprites.Length)];
 			toInstantiate.transform.position = new Vector3 (Random.Range (-9f, 9f), Random.Range (-5f, 5f), 0f);
 			toInstantiate.transform.localScale = new Vector3 (Random.Range (0f, 2f), Random.Range (0f, 2f), 0f);
 			Destroy(toInstantiate, Random.Range(5f, 10f));
