76139aaef7dea80e0fed2b7acfb97d5be2376ec7
AntoineDutil <antoineberthiaumedutil@gmail.com>
2016-10-31 14:15:48 -0400

    Fini l'interface.

---
+++ Assets/Scripts/UIModuleController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIModuleController : MonoBehaviour {
+
+    public int nbrRessources;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if(GameManager.instance.Ressource < nbrRessources)
+        {
+            GetComponent<Image>().color = new Color32(128, 128, 128, 255);
+        }
+        else
+        {
+            GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+        }
+	}
+}
