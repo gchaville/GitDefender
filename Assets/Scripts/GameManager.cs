86f49c2734efc8e670e0346d7b236adfe1caa867
antronik7 <antoineberthiaumedutil@gmail.com>
2016-11-01 18:11:20 -0400

    Changer les point de vie de la branche.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -69,7 +69,7 @@ public class GameManager : MonoBehaviour {
             sm.StartNewWave(wave);
             yield return new WaitWhile(() => monstersLeft > 0);
 			instantiateUINewWave ("wave over", 0);
-            yield return new WaitForSeconds(4f);
+            yield return new WaitForSeconds(3f);
         }
     }
 
